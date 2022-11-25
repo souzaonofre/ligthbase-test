e1a982a96a0c95284c05fde6eff5568443c38553
Onofre Souza <souzaonofre@gmail.com>
2022-11-25 06:51:20 -0300

    Customizando controller p/ refletir rotas e especificao

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -5,73 +5,90 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreClienteRequest;
 use App\Http\Requests\UpdateClienteRequest;
 use App\Models\Cliente;
+use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+
+    // /**
+    //  * Display a listing of the resource.
+    //  *
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function index()
+    // {
+    //     //
+    // }
 
     /**
-     * Store a newly created resource in storage.
+     * Cadastro novo Cliente.
      *
      * @param  \App\Http\Requests\StoreClienteRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreClienteRequest $request)
+    public function novoCliente(StoreClienteRequest $request)
     {
         //
     }
 
     /**
-     * Display the specified resource.
+     * Consulta dados de um Cliente
      *
-     * @param  \App\Models\Cliente  $cliente
+     * @param  \\Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Cliente $cliente)
+    public function consultarCliente(Request $request, int $id)
     {
         //
     }
 
     /**
-     * Edit the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateClienteRequest  $request
-     * @param  \App\Models\Cliente  $cliente
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(UpdateClienteRequest $request, Cliente $cliente)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
+     * Edicao de um Cliente existente.
      *
      * @param  \App\Http\Requests\UpdateClienteRequest  $request
-     * @param  \App\Models\Cliente  $cliente
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateClienteRequest $request, Cliente $cliente)
+    public function edit(UpdateClienteRequest $request, int $id)
+    {
+        //
+    }
+
+    // /**
+    //  * Update the specified resource in storage.
+    //  *
+    //  * @param  \App\Http\Requests\UpdateClienteRequest  $request
+    //  * @param  \App\Models\Cliente  $cliente
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function update(UpdateClienteRequest $request, Cliente $cliente)
+    // {
+    //     //
+    // }
+
+    /**
+     * Remocao de um Cliente existente.
+     *
+     * @param  \\Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function removerCliente(Request $request, int $numero)
     {
         //
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Consulta dados de Clientes onde ultimo numero placa seja igual informado.
      *
-     * @param  \App\Models\Cliente  $cliente
+     * @param  \\Illuminate\Http\Request $request
+     * @param string $numero
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Cliente $cliente)
+    public function consultarPlaca(Request $request, string $numero)
     {
         //
     }
+
 }
