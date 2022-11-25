b15fbd4b15e61a4e018c969b934713a4a420e06c
Onofre Souza <souzaonofre@gmail.com>
2022-11-25 07:08:22 -0300

    Criando migration tabela clientes e config. o model Cliente

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class Cliente extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'clientes';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'nome',
+        'telefone',
+        'cpf',
+        'placa_carro'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
 }
